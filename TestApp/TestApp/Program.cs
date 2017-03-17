40483305e2c7d21f73c5c7deb3803688b94bb58e
boyPhysicist <fillhome@mail.ru>
2017-03-17 23:25:04 +0300

    Пробный заход

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double q = 0;
+            while (q != 10)
+            {
+                Console.WriteLine("Hello world");
+                q+=0.1;
+            }
+        }
+    }
+}

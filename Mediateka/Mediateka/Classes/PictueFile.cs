93433a56453e60752c8183b86323a1a49cf943c8
boyPhysicist <fillhome@mail.ru>
2017-04-09 12:25:59 +0300

    commit3

---
+++ Mediateka/Mediateka/Classes/PictueFile.cs
@@ -0,0 +1,34 @@
+﻿using Mediateka.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Classes
+{
+    public class PictueFile : MediaFile, IResolution, IType
+    {
+        public int[] Resolution
+        {
+            get;
+            
+        }
+
+        public string Type
+        {
+            get;
+            
+        }
+
+
+
+        public PictueFile(string name, double size, string url, string author, int[] resolution, string type) : base(name, size, url, author)
+        {
+            Resolution = resolution;
+            Type = type;
+        }
+
+       
+    }
+}

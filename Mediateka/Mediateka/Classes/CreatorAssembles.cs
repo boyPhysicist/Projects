06bf3e552ff54c68ab00b18e33324cf73f9c3579
boyPhysicist <fillhome@mail.ru>
2017-04-14 17:33:06 +0300

    Создание колекций(надо сделать проверку на соответствие элементов во входящей коллекции)

--- Mediateka/Mediateka/Classes/CreatorAssembles.cs
+++ Mediateka/Mediateka/Classes/CreatorAssembles.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Mediateka.Classes
 {
     public class CreatorAssembles
-    { 
+    {
+        public  ICollection<MediaFile> CreateDisk(ICollection<MediaFile> w, MediaFile mediaFile)
+        {
+
+            MediaFile M = mediaFile;
+            ICollection<MediaFile> W = w;
+            if (W != null)
+            {
+                int s = w.Count();
+                
+            }
+            if(M is AudioFile|| M is PictureFile)
+            { W.Add(M);
+                Console.WriteLine("{0} Добавлен", M.Name);
+            }
+            else { Console.WriteLine("Error"); };
+           
+            return w;
+
+            
+        }
+
+        public CreatorAssembles() {
+           
+        }
+         
     }
 }
--- Mediateka/Mediateka/Program.cs
+++ Mediateka/Mediateka/Program.cs
@@ -15,16 +15,20 @@ namespace Mediateka
             int[] a = new int[2];
             a[0] = 1024;
             a[1] = 768;
-            ICollection<IMediaFileItems> k = new List<IMediaFileItems>;
+            
             PictureFile p = new PictureFile("picture", 123, "jsjsjs", "phill", a, "jpeg");
             PictureFile p1 = new PictureFile("picture", 123, "jsjsjs", "phill", a, "jpeg");
             PictureFile p2 = new PictureFile("picture", 123, "jsjsjs", "phill", a, "jpeg");
             PictureFile p3 = new PictureFile("picture", 123, "jsjsjs", "phill", a, "jpeg");
             AudioFile b = new AudioFile("audio", 123, "jsjs", "phill", 256, "mp3", 120);
-            k.Add(p);
-            Event e = new Event(k,"kkkkk");
+            VideoFile v = new VideoFile("video", 124, "jdjfjf", "asd", 212, a, "avi", 123);
 
-
+            ICollection<MediaFile> k = new List<MediaFile>();
+            CreatorAssembles z = new CreatorAssembles();
+            var i = z.CreateDisk(k, p);
+            i = z.CreateDisk(k, b);
+            i = z.CreateDisk(k, v);
+            Console.WriteLine(i.Count);
             
             Console.WriteLine();
             Console.ReadKey();

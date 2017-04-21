047bb3e271f994f4a4fa5e274ec1d0ecf14d631c
philip <fillhome@mail.ru>
2017-04-21 14:26:32 +0300

    DataCargoPlaneLoader

--- Airline/Airline/DataLoader/DataCargoPlaneLoader.cs
+++ Airline/Airline/DataLoader/DataCargoPlaneLoader.cs
@@ -10,11 +10,30 @@ namespace Airline.DataLoader
 {
     public class DataCargoPlaneLoader : DataLoader
     {
-        public override CargoPlane[] GetData(string filePath)
+        public override AirplaneModel[] GetData(string filePath)
         {
             string _path = filePath;
             string[] temp = File.ReadAllLines(_path);
+            char _delimiterChar = ',';
+            CargoPlane[] CP=new CargoPlane[temp.Length];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                string[] words = temp[i].Split(_delimiterChar);
+                if(words.Length == 18)
+                { 
+                    CP[i] = new CargoPlane(words[0], words[1], Convert.ToDouble(words[2]), Convert.ToDouble(words[3]),
+                        Convert.ToDouble(words[4]), Convert.ToDouble(words[5]), Convert.ToDouble(words[6]), Convert.ToDouble(words[7]),
+                        Convert.ToDouble(words[8]), Convert.ToDouble(words[9]), Convert.ToDouble(words[10]), Convert.ToDouble(words[11]),
+                        Convert.ToDouble(words[12]), Convert.ToDouble(words[13]), Convert.ToDouble(words[14]), Convert.ToDouble(words[15]),
+                        words[16], Convert.ToInt32(words[17]));
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
 
+            return CP;
         }
     }
 }
--- Airline/Airline/DataLoader/DataLoader.cs
+++ Airline/Airline/DataLoader/DataLoader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Airline.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace Airline.DataLoader
 {
     public abstract class DataLoader
     {
-        public string FilePath { get; }
-        public abstract void GetData(string filePath);
+   
+        public abstract AirplaneModel[] GetData(string filePath);
     }
 }

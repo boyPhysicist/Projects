70c1af3990af17983499c4419b0781057e9d84d4
Philip <fillhome@mail.ru>
2017-05-08 23:48:01 +0300

    delimeter and separator

---
+++ TextHandler/TextHandler/Classes/SentenceDelimeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextHandler.Interfaces;
+
+namespace TextHandler.Classes
+{
+    public class SentenceDelimeter : IDelimeter
+    {
+        private string[] sentenceDelimeters = new string[] {".","!","?","..." };
+        public IEnumerable<string> Delimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TextHandler/TextHandler/Classes/WordSeparators.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextHandler.Interfaces;
+
+namespace TextHandler.Classes
+{
+    public class WordSeparators : IDelimeter
+    {
+        private string[] wordSeparators = new string[] {" ","-",", " };
+        public IEnumerable<string> Delimeter()
+        {
+            return wordSeparators.AsEnumerable();
+        }
+    }
+}

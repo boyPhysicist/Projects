0ebc75c7bcfc191b4fa17b43a11f1c0db4100b94
philip <fillhome@mail.ru>
2017-06-09 16:27:43 +0300

    "Тихо...Очень тихо..."

---
+++ Task4/Task4.DM/Entities/EntityState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.DM.Entities
+{
+    public enum EntityState
+    {
+        Unchanged,
+        Added,
+        Modified,
+        Deleted
+
+    }
+}
